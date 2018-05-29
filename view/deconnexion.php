e8f11aec3cd852eba0bc07f642c6316624635435
maxcrn <maxcrn@icloud.com>
2018-05-29 12:07:35 +0200

    Redirection rechercheEquipeTournoi

--- view/deconnexion.php
+++ view/deconnexion.php
@@ -1,4 +1,4 @@
 <html>
 <h3> Vous avez bien été déconnecté ! Merci de votre visite ! A bientôt ! </h3>
-<a href="accueil">Retourner à la page d'accueil </a>
+<a href="/">Retourner à la page d'accueil </a>
 </html>
